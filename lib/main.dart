f20a8e201977e130d020c6ecbef22c34b491a79c
lucasbqf <lucasbqf@hotmail.com>
2021-11-08 15:48:01 -0300

    apagado algumas linhas

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Webview',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+
       ),
       home: SafeArea(child:WebViewer()),
     );
