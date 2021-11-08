6c2efab193ca85fbba4e860005ec670487648d38
lucasbqf <lucasbqf@hotmail.com>
2021-11-08 11:22:11 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Webview',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SafeArea(child:WebViewer()),
+    );
+  }
+}
+
+class WebViewer extends StatefulWidget {
+  const WebViewer({Key? key}) : super(key: key);
+  @override
+  WebViewerState createState() => WebViewerState();
+}
+
+class WebViewerState extends State<WebViewer> {
+  final url='https://google.com';
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      allowsInlineMediaPlayback: true,
+      gestureNavigationEnabled: true,
+      javascriptMode: JavascriptMode.unrestricted,
+      initialUrl: url
+    );
+  }
+}
